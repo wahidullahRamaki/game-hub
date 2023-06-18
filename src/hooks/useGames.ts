cdeff4862261b1bbdf20fd3db8ba0975ea5739ca
wahidullahRamaki <wahidullah.ramaki@gamil.com>
2023-06-18 15:23:44 +0430

    Building game card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import apiClient from "../Services/api-client";
+import { CanceledError } from "axios";
 
-interface Game {
-    id: number,
-    name: string
+ export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -24,7 +26,7 @@ const useGames = () => {
             .catch(err => {
 
                 // i put cancelledcallback instead canceledError
-                if (err instanceof cancelIdleCallback) return;
+                if (err instanceof CanceledError) return;
                 setError(err.message)
             })
 
