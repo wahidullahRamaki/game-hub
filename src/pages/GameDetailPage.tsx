38d7bb30adad1a2176da061ac989a1441ef9d781
wahidullahRamaki <wahidullah.ramaki@gamil.com>
2023-08-09 15:47:11 +0430

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 const GameDetailPage = ()=>{
     const {slug}= useParams();
     const {data:game,isLoading,error}=useGame(slug!);
@@ -9,7 +10,7 @@ const GameDetailPage = ()=>{
     return (
         <>
         <Heading>{game.name}</Heading>
-        <Text>{game.description_raw}</Text>
+        <ExpandableText>{game.description_raw}</ExpandableText>
         </>
 
     )
