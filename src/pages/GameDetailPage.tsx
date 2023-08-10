24390e8673880f63282f68965caacf8409b36337
wahidullahRamaki <wahidullah.ramaki@gamil.com>
2023-08-10 10:56:53 +0430

    build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttirbutes";
 const GameDetailPage = ()=>{
     const {slug}= useParams();
     const {data:game,isLoading,error}=useGame(slug!);
@@ -11,6 +14,7 @@ const GameDetailPage = ()=>{
         <>
         <Heading>{game.name}</Heading>
         <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game}/>
         </>
 
     )
